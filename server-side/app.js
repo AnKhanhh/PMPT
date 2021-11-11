01d59d7d51c6c422eaf405155f1bcde3294a8d76
AnKhanhh <https://github.com/AnKhanhh/PMPT>
2021-11-11 20:10:44 +0700

    add connect and disconnect event

--- server-side/app.js
+++ server-side/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const http = require('http');
 const io = require('socket.io')(server);
+const gameLogic = require('./processes');
 const app = express();
 
 const server = http.createServer(app);
 
-io.on('connection',);
+io.on('connection',(client) => {
+	gameLogic.initGame(io, client);
+});
 
 server.listen(process.env.PORT || 8000);
--- server-side/processes.js
+++ server-side/processes.js
@@ -1,11 +1,23 @@
 var io;
 var gameSocket;
-// store all active socket connections
+// store all active socket connections in an array
 var activeSession = [];
 
 const initGame = (sio, socket) => {
 	io = sio;
 	gameSocket = socket;
+	activeSession.push(gameSocket);
+	gameSocket.on('Disconnect', handleDisconnect);
+	gameSocket.on('createNewGame', handleCreateSession);
 }
 
+function handleDisconnect() {
+	let i = activeSession.indexOf(gameSocket);
+	activeSession.splice(i, 1);
+}
+function handleCreateSession(gameId) {
+// send room id and socket id back to client, for later uses
+	this.emit('createNewGame', {gameId: gameId, mySocketId: this.id});
+	this.join(gameId);
+}
 exports.initGame = initGame;
