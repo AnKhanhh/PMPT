188048917066ad42178e0b3f22de5368e8882600
AnKhanhh <https://github.com/AnKhanhh/PMPT>
2021-12-12 19:02:21 +0700

    update app

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,37 +1,62 @@
-import React, { Component } from 'react';
+import React from "react";
 // import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import Onboard from './board/onboard';
-import { ColorContext } from './context/colorcontext';
-import JoinGame from './board/joingame';
+// import './App.css';
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch,
+} from "react-router-dom";
+import Onboard from "./board/onboard";
+import { ColorContext } from "./context/colorcontext";
+import JoinRoom from "./board/joinroom";
+import JoinGame from "./board/joingame";
+import ChessGame from ".ui/chessgame";
 
 function App() {
+    const [didRedirect, setDidredirect] = React.useState(false);
 
-	const [didRedirect, setDidredirect] = React.useState(false)
+    const playerDidRedirect = React.useCallback(() => {
+        setDidredirect(true);
+    }, []);
 
-	const [userName, setUserName] = React.useState('');
+    const playerDidNotRedirect = React.useCallback(() => {
+        setDidredirect(false);
+    }, []);
 
-  return (
-	<ColorContext.Provider value={ {didRedirect: didRedirect, playerDidNotRedirect: playerDidNotRedirect, playerDidRedirect: playerDidRedirect} }>
-		<Router>
-			<Switch>
-				<Route exact path='/'>
-					<Onboard setUserName={setUserName} />
-				</Route>
-				<Route exact path='/game/:gameid'>
-					{/* {didRedirect?
-						<>
-							<JoinGame userName={userName} isCreator={true}/>
-						</>
-					:
-						<JoinRoom />
-					} */}
-				</Route>
-			</Switch>
-			</Router>
-	</ColorContext.Provider>
-  );
+    const [userName, setUserName] = React.useState("");
+
+    return (
+        <ColorContext.Provider
+            value={{
+                didRedirect: didRedirect,
+                playerDidNotRedirect: playerDidNotRedirect,
+                playerDidRedirect: playerDidRedirect,
+            }}
+        >
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Onboard setUserName={setUserName} />
+                    </Route>
+                    <Route exact path="/game/:gameid">
+                        {didRedirect ? (
+                            <>
+                                <JoinGame
+                                    userName={userName}
+                                    isCreator={true}
+                                />
+                                <ChessGame myUserName={userName} />
+                            </>
+                        ) : (
+                            <JoinRoom />
+                        )}
+                    </Route>
+                    <Redirect to="/" />
+                </Switch>
+            </Router>
+        </ColorContext.Provider>
+    );
 }
 
 export default App;
