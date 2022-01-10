bb46fe0dafec8f660fc04674a0d7bc405c3b89ce
AnKhanhh <https://github.com/AnKhanhh/PMPT>
2022-01-10 21:19:05 +0700

    fix dockerfile

--- server-side/app.js
+++ server-side/app.js
@@ -12,6 +12,6 @@ io.on("connection", (client) => {
 });
 
 server.listen(process.env.PORT || 8000);
-// app.get('/', (req, res) => {
-// 	res.send('listening on '  + (process.env.PORT || 8000))
-// });
+app.get('/', (req, res) => {
+	res.send('listening on '  + (process.env.PORT || 8000))
+});
